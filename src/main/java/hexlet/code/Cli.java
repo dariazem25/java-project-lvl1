c746601154047a4184352e9caa4ca964e1f68863
Daria Zemlyanaya <zemlyanayadasha78@gmail.com>
2021-12-12 23:07:25 +0300

    checkstyle

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cli {
 
     public static void greeting() {
-        System.out.print("Welcome to the Brain Games! \nMay I have your name?\s");
+        System.out.print("Welcome to the Brain Games! \nMay I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
         System.out.println("Hello, " + name + "!");
